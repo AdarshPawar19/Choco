dca53df3e419c274b0bee553931c9b10cbe6f526
AdarshPawar19 <pawar.adarsh0111@gmail.com>
2024-10-06 13:46:09 +0530

    created product endpoint

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,39 @@
+import { db } from "@/lib/db/db";
+import { products } from "@/lib/db/schema";
+import { productsSchema } from "@/lib/validators/productSchema";
+import { writeFile } from "node:fs/promises";
+import path from "node:path";
+
+export async function  POST(request:Request) {
+    const data = await request.formData();
+    let validatedData;
+    try{
+        validatedData=  productsSchema.parse({
+            name:data.get('name'),
+            description:data.get('description'),
+            price:Number(data.get('price')),
+            image:data.get('image')
+        })
+    }catch(err){
+        return Response.json({message:err},{status:400});
+    }
+
+    const filename=`${Date.now()}.${validatedData.image.name.split(".").slice(-1)}`;  //original name: choco.png 
+    // 212313434.png we want
+
+    try {
+        const buffer=Buffer.from(await validatedData.image.arrayBuffer());
+        await writeFile(path.join(process.cwd(),"public/assets",filename),buffer);
+    } catch (error) {
+        return Response.json({message:"Failed to save the file to fs"},{status:500})
+    }
+
+    try {
+        await db.insert(products).values({...validatedData ,image:filename })
+    } catch (error) {
+        //todo:remove stored iamge from fs
+        return Response.json({message:"Failed to store product into database"},{status:500})
+    }
+
+    return Response.json({message:"OK"},{status:201});
+}

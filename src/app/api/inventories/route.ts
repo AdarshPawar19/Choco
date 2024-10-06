c66872cd1c9329c84b7a1cd3ebf12b0deb019e50
AdarshPawar19 <pawar.adarsh0111@gmail.com>
2024-10-06 16:47:07 +0530

    fetch all inventories endpoint

--- src/app/api/inventories/route.ts
+++ src/app/api/inventories/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db/db";
-import { inventories } from "@/lib/db/schema";
+import { inventories, products, warehouses } from "@/lib/db/schema";
 import { inventorySchema } from "@/lib/validators/InventorySchema";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(request:Request) {
     const requestData=await request.json();
@@ -16,5 +17,24 @@ export async function POST(request:Request) {
         return Response.json({message:"OK"},{status:201});
     } catch (error) {
         return Response.json({message:"Failed to store inventory in db."},{status:500})
+    }
+}
+
+export async function GET() {
+    try {
+       const allInventories= await db.select({
+        id:inventories.id,
+        sku:inventories.sku,
+        warehouse:warehouses.name,
+        product:products.name
+       }).from(inventories)
+       .leftJoin(warehouses,eq(inventories.warehouseId,warehouses.id))
+       .leftJoin(products,eq(inventories.productId,products.id))
+       .orderBy(desc(inventories.id));
+
+       return Response.json(allInventories);
+
+    } catch (error) {
+        return Response.json({message:"Failed to fetch inventories"},{status:500})
     }
 }

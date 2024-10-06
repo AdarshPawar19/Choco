3aaec79067bde2151ee3f225891c359fff50a1a4
AdarshPawar19 <pawar.adarsh0111@gmail.com>
2024-10-06 19:28:36 +0530

    added authentication

---
+++ src/lib/auth/authOptions.ts
@@ -0,0 +1,54 @@
+import GoogleProvider from "next-auth/providers/google"
+import { db } from "../db/db";
+import { users } from "../db/schema";
+import { callbackify } from "util";
+import { AuthOptions } from "next-auth";
+
+export const authOptions :AuthOptions={
+    providers:[
+        GoogleProvider({
+            clientId:process.env.GOOGLE_CLIENT_ID!,
+            clientSecret:process.env.GOOGLE_CLIENT_SECRET!  , 
+            async profile(profile,token:any){
+                console.log("profile" , profile)
+                console.log("token" , token);
+
+                const data ={
+                    fname:profile.given_name,
+                    lname:profile.family_name,
+                    email:profile.email,
+                    provider:"GOOGLE",
+                    externalId:profile.sub,
+                    image:profile.picture
+                }
+                try {
+                    const user =await db.insert(users).values(data).onConflictDoUpdate({target:users.email,set:data}).returning();
+                    return{
+                        ...data,
+                        name:data.fname,
+                        id:String(user[0].id),
+                        role:user[0].role
+                    }
+                } catch (error) {
+                    console.log(error);
+                    return {
+                        id:""
+                    }
+                }
+            }
+        }),
+
+    ],
+    callbacks:{
+        session(data:any){
+            return data
+        },
+        jwt({token,user}:{token:any;user:any}){
+            if(user){
+                token.role = user.role
+                token.id=user.id
+            }
+            return token;
+        }
+    }
+}

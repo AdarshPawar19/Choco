a0bed9de5d0f80c2022c8695b356c753e81ab37f
AdarshPawar19 <pawar.adarsh0111@gmail.com>
2024-10-06 16:11:49 +0530

    create all devlieryperson details endpoint

--- src/app/api/delivery-persons/route.ts
+++ src/app/api/delivery-persons/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db/db";
-import { deliveryPersons } from "@/lib/db/schema";
+import { deliveryPersons, warehouses } from "@/lib/db/schema";
 import { deliveryPersonsSchema } from "@/lib/validators/deliveryPersonSchema";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(request:Request) {
     const requestData=await request.json();
@@ -16,5 +17,26 @@ export async function POST(request:Request) {
         return Response.json({message:"OK"},{status:201})
     } catch (error) {
         return Response.json({message:"Failed to store the delivery person into database"},{status:500})
+    }
+}
+
+
+export async function GET() {
+    try {
+       const alldeliverPerson = await db
+       .select({
+        id:deliveryPersons.id,
+        name:deliveryPersons.name,
+        phone:deliveryPersons.phone,
+        warehouse:warehouses.name,   
+       })
+       .from(deliveryPersons)
+       .leftJoin(warehouses , eq(deliveryPersons.warehouseId , warehouses.id))
+       .orderBy(desc(deliveryPersons.id));
+
+       return Response.json(alldeliverPerson);
+
+    } catch (error) {
+        return Response.json({message:"Failed to fetch delivery persons"},{status :500 })
     }
 }

a858c222e269b8df0ffb173427c37b2a7485ef5e
AdarshPawar19 <pawar.adarsh0111@gmail.com>
2024-10-06 20:15:40 +0530

    added shadcn library

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button';
 import React from 'react'
 
 function AdminPage() {
   return (
     <div>
       <h1>hello admin</h1>
+      <Button>Click me</Button>
     </div>
   )
 }

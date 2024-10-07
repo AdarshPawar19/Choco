75460acd45b255e23bbd5ed82cd98cd9deca8857
AdarshPawar19 <pawar.adarsh0111@gmail.com>
2024-10-07 09:53:02 +0530

    close drawer automatically once put data in form

--- src/app/admin/products/product-sheet.tsx
+++ src/app/admin/products/product-sheet.tsx
@@ -14,6 +14,7 @@ function ProductSheet() {
         onSuccess:()=>{
             queryClient.invalidateQueries({queryKey:['products']}),
             alert("Product created!")
+            onClose()
         }
     })
 
